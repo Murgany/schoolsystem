d1af5314b5b2356977cef5eaeebb613c024e879b
Murgany <76653393+Murgany@users.noreply.github.com>
2023-01-08 16:42:06 +0200

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: "http://simpleschoolsystem.pythonanywhere.com"
+    baseURL: "https://simpleschoolsystem.pythonanywhere.com"
     // baseURL: "http://127.0.0.1:8000/api/"
 })
 
